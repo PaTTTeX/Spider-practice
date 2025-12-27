60db08861ebd18dd26ff3a8cd9fd08529874d4af
G00406870 <GOO406870@ATU.IE>
2025-12-27 01:30:24 +0000

    practicing on using images in a list and displaying it.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function IsloggedIn(props) {
   return (props.loggedIn ? <h2>Welcome back, {props.user}!</h2> : <h2>Please Log in</h2>)
 }
 
-function ButtonE(){
+function ButtonE() {
   const buttonHandle = (e) => e.target.textContent = "YahoooOuch"
 
   return <button onClick={buttonHandle}>Click me</button>
@@ -16,41 +16,49 @@ function List(props) {
   const category = props.category;
 
 
-  const showList = items.map(products => <li key={products.name}>
-    Fruit: <b>{products.name}</b>
-    Type: <b>{products.type}</b>
-  </li>);
+  const showList = items.map(products =>
+    <li className='border p-5 w-1/2' key={products.id}>
+      <div className='text-center'>
+        <ul>
+          Fruit: <b>{products.name}</b>
+        </ul>
+        <ul>
+          Type: <b>{products.type}</b>
+        </ul>
+      </div>
+      <img className='w-[50%] h-auto flex m-auto my-3' src={products.image} />
+    </li>);
 
   return (
     <>
-      <h2 className='text-3xl bg-white text-center text-black'>{category}</h2> <ol>{showList}</ol>
+      <h2 className='text-3xl bg-white text-center text-black'>{category}</h2>
+      <ol className='flex'>{showList}</ol>
     </>)
 }
 
 const fruit = [
-  { name: "apple", type: "fruit" },
-  { name: "orange", type: "fruit" },
-  { name: "banana", type: "fruit" }
+  { id: 1, name: "Apple", type: "Fruit", image: "/images/spider-logo.png" },
+  { id: 2, name: "Orange", type: "Fruit", image: "apple" },
+  { id: 3, name: "Banana", type: "Fruit", image: "apple" }
 ];
 
-const Spider = [
-  { name: "Elizabeth", type: "fruit" },
-  { name: "Goliath", type: "fruit" },
-  { name: "banana", type: "fruit" }
-];
+const pets = [
+  { name: "Cheese" },
+  { name: "Pepe" },
+]
 
 function App() {
   return (
     <>
       <Header />
       <main className="main mx-10">
+        <h3 className='flex text-center justify-between'>{pets.map(e => <ul className='bg-white text-black w-[50%]' key={e.name}>{e.name}</ul>)}</h3>
         <h1 className="text-3xl font-bold underline">
         </h1>
         <IsloggedIn loggedIn={false} user="Tomas" />
         <ul className='list-disc'>
           <button onClick={() => console.log("You clicked horray")}>Button</button>
           <List items={fruit} category="FRUITS" />
-          <List items={Spider} category="SPIDERS" />
         </ul>
         <ButtonE />
       </main>
